52b7458cdec5427d3c8a25a50bbf1910f352d516
Tushar Kaundal <kaundaltushar@gmail.com>
2025-04-07 11:12:04 +0530

    completed and added animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,320 @@
+import Navbar from "./component/Navbar/Navbar";
+import React from "react";
+import FacebookIcon from "./assets/icon-facebook.svg?react";
+import PinterestIcon from "./assets/icon-pinterest.svg?react";
+import InstagramIcon from "./assets/icon-instagram.svg?react";
+import TwitterIcon from "./assets/icon-twitter.svg?react";
+
 function App() {
-  return <div className="bg-red-500">Vite and React logos to learn more</div>;
+  return (
+    <>
+      <main className="h-full w-full">
+        <div className="relative text-white ">
+          <Navbar />
+          <div className="w-full h-full absolute  flex flex-col justify-center items-center font-fraunces">
+            <h1 className="text-wrap text-center text-[40px] md:text-[56px] tracking-[8.75px] font-bold animate-movedown">
+              WE ARE CREATIVES
+            </h1>
+            <div className=" mt-18 sm:mt-10 animate-movedown hover:animate-bounce">
+              <svg
+                width="36"
+                height="114"
+                viewBox="0 0 36 114"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="Group">
+                  <path
+                    id="Path 7"
+                    d="M18 3V103"
+                    stroke="white"
+                    strokeWidth="6"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    id="Path 8"
+                    d="M3 95.4843L18 110.484L33 95.4843"
+                    stroke="white"
+                    strokeWidth="6"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+              </svg>
+            </div>
+          </div>
+          <picture>
+            <source
+              media="(min-width:640px)"
+              srcSet="/images/desktop/image-header.jpg"
+            />
+            <img
+              className="w-full sm:h-screen sm:object-cover"
+              src="/images/mobile/image-header.jpg"
+              alt="orange_background"
+            />
+          </picture>
+        </div>
+
+        <section className="grid grid-cols-1 md:grid-cols-[repeat(2,minmax(0,50%))] font-fraunces">
+          <picture className="md:order-2">
+            <source
+              media="(min-width:768px)"
+              srcSet="/images/desktop/image-transform.jpg"
+            />
+            <img
+              className="w-full h-full"
+              src="/images/mobile/image-transform.jpg"
+              alt="orange_background"
+            />
+          </picture>
+          <div className="md:order-1 w-full flex justify-center md:justify-start">
+            <div className="flex flex-col text-center md:text-start justify-center items-center md:items-start px-6 py-16 gap-10 max-w-[500px]">
+              <h1 className="text-[40px] text-[#24303E] font-bold tracking-[-0.29px] animate-movedown">
+                Transform your brand
+              </h1>
+              <p className="font-barlow font-semibold text-[#808397] leading-7 tracking-[-0.13px] animate-[moveDown_4s]">
+                We are a full-service creative agency specializing in helping
+                brands grow fast. Engage your clients through compelling visuals
+                that do most of the marketing for you.
+              </p>
+              <a
+                className="relative w-fit font-bold text-[#24303E] leading-6 tracking-[1px]  before:absolute before:-left-1 before:bottom-0 before:-z-1 before:content-[' '] before:w-[110px] before:h-[10px] before:rounded-3xl before:bg-yellow-100 hover:before:bg-yellow-300 animate-[moveDown_1s_2s]"
+                href="#"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+
+          <picture className="md:order-3">
+            <source
+              media="(min-width:768px)"
+              srcSet="/images/desktop/image-stand-out.jpg"
+            />
+            <img
+              className="w-full h-full"
+              src="/images/mobile/image-stand-out.jpg"
+              alt="orange_background"
+            />
+          </picture>
+          <div className="md:order-4 w-full flex justify-center md:justify-start">
+            <div className="flex flex-col text-center md:text-start justify-center items-center md:items-start px-6 py-16 gap-10 max-w-[500px]">
+              <h1 className="text-[40px] text-[#24303E] font-bold tracking-[-0.29px]">
+                Stand out to the right audience
+              </h1>
+              <p className="font-barlow font-semibold text-[#808397] leading-7 tracking-[-0.13px]">
+                Using a collaborative formula of designers, researchers,
+                photographers, videographers, and copywriters, we’ll build and
+                extend your brand in digital places.
+              </p>
+              <a
+                className="relative w-fit font-bold text-[#24303E] leading-6 tracking-[1px]  before:absolute before:-left-1 before:bottom-0 before:-z-1 before:content-[' '] before:w-[110px] before:h-[10px] before:rounded-3xl before:bg-red-300 hover:before:bg-red-500"
+                href="#"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+
+          <div className="md:order-5 relative">
+            <div className="absolute bottom-8 w-full flex justify-center items-center text-center">
+              <div className="max-w-85">
+                <h1 className="font-fraunces text-3xl font-bold text-[#24554A] mb-5">
+                  Graphic Design
+                </h1>
+                <p className="font-barlow font-semibold leading-7 tracking-[-0.11px] text-[#3E7467]">
+                  Great design makes you memorable. We deliver artwork that
+                  underscores your brand message and captures potential clients’
+                  attention.
+                </p>
+              </div>
+            </div>
+            <picture>
+              <source
+                media="(min-width:768px)"
+                srcSet="/images/desktop/image-graphic-design.jpg"
+              />
+              <img
+                className="w-full max-h-150 object-cover object-center"
+                src="/images/mobile/image-graphic-design.jpg"
+                alt="orange_background"
+              />
+            </picture>
+          </div>
+          <div className="md:order-6 relative">
+            <div className="absolute bottom-8 w-full flex justify-center items-center text-center">
+              <div className="max-w-85">
+                <h1 className="font-fraunces text-3xl font-bold text-[#052C3B] mb-5">
+                  Photography
+                </h1>
+                <p className="font-barlow font-semibold leading-7 tracking-[-0.11px] text-[#19536C]">
+                  Increase your credibility by getting the most stunning,
+                  high-quality photos that improve your business image.
+                </p>
+              </div>
+            </div>
+            <picture>
+              <source
+                media="(min-width:768px)"
+                srcSet="/images/desktop/image-photography.jpg"
+              />
+              <img
+                className="w-full max-h-150 object-cover object-center"
+                src="/images/mobile/image-photography.jpg"
+                alt="orange_background"
+              />
+            </picture>
+          </div>
+        </section>
+        <section className="px-6 py-16 md:py-40 text-center flex flex-col justify-center items-center">
+          <h1 className="mb-16 font-fraunces font-bold tracking-[5px] uppercase text-[#A7AAAD]">
+            Client testimonials
+          </h1>
+          <div className="grid md:grid-cols-[repeat(3,minmax(0,340px))] gap-16 md:gap-8">
+            <div className="flex flex-col items-center gap-8 font-barlow">
+              <img
+                className="w-18 h-18 rounded-[50%]"
+                src="/images/image-emily.jpg"
+                alt=""
+              />
+              <p className="font-semibold leading-6 md:leading-8 tracking-[-0.13px] text-[#5B636D]">
+                We put our trust in Sunnyside and they delivered, making sure
+                our needs were met and deadlines were always hit.
+              </p>
+              <div>
+                <p className="font-fraunces font-bold">Emily R.</p>
+                <p className="font-semibold text-[14px] leading-7 tracking-[-0.1px] text-[#A7AAAD]">
+                  Marketing Director
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center gap-8 font-barlow">
+              <img
+                className="w-18 h-18 rounded-[50%]"
+                src="/images/image-jennie.jpg"
+                alt=""
+              />
+              <p className="font-semibold leading-6 md:leading-8 tracking-[-0.13px] text-[#5B636D]">
+                Sunnyside’s enthusiasm coupled with their keen interest in our
+                brand’s success made it a satisfying and enjoyable experience.
+              </p>
+              <div>
+                <p className="font-fraunces font-bold">Thomas S.</p>
+                <p className="font-semibold text-[14px] leading-7 tracking-[-0.1px] text-[#A7AAAD]">
+                  Chief Operating Officer
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center gap-8 font-barlow">
+              <img
+                className="w-18 h-18 rounded-[50%]"
+                src="/images/image-thomas.jpg"
+                alt=""
+              />
+              <p className="font-semibold leading-6 md:leading-8 tracking-[-0.13px] text-[#5B636D]">
+                Incredible end result! Our sales increased over 400% when we
+                worked with Sunnyside. Highly recommended!
+              </p>
+              <div>
+                <p className="font-fraunces font-bold">Jennie F.</p>
+                <p className="font-semibold text-[14px] leading-7 tracking-[-0.1px] text-[#A7AAAD]">
+                  Business Owner
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="grid grid-rows-2 grid-cols-2 md:grid-cols-4 md:grid-rows-none">
+          <div>
+            <picture>
+              <source
+                media="(min-width:768px)"
+                srcSet="/images/desktop/image-gallery-milkbottles.jpg"
+              />
+              <img
+                src="/images/mobile/image-gallery-milkbottles.jpg"
+                alt="orange_background"
+              />
+            </picture>
+          </div>
+          <div>
+            <picture>
+              <source
+                media="(min-width:768px)"
+                srcSet="/images/desktop/image-gallery-orange.jpg"
+              />
+              <img
+                src="/images/mobile/image-gallery-orange.jpg"
+                alt="orange_background"
+              />
+            </picture>
+          </div>
+          <div>
+            <picture>
+              <source
+                media="(min-width:768px)"
+                srcSet="/images/desktop/image-gallery-cone.jpg"
+              />
+              <img
+                src="/images/mobile/image-gallery-cone.jpg"
+                alt="orange_background"
+              />
+            </picture>
+          </div>
+          <div>
+            <picture>
+              <source
+                media="(min-width:768px)"
+                srcSet="/images/desktop/image-gallery-sugarcubes.jpg"
+              />
+              <img
+                src="/images/mobile/image-gallery-sugar-cubes.jpg"
+                alt="orange_background"
+              />
+            </picture>
+          </div>
+        </section>
+      </main>
+
+      <footer className="w-full flex justify-center items-center flex-col bg-[#90D4C6] font-barlow">
+        <div className="flex flex-col justify-center items-center gap-10 max-w-75 mx-10 my-16 md:my-18">
+          <h1 className="text-[#2C7566] text-4xl font-bold">sunnyside</h1>
+          <div className="flex gap-10 [&>a]:hover:text-white font-semibold text-[#458D7E]">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Projects</a>
+          </div>
+          <div className="flex gap-7 [&>svg]:hover:text-white text-[#2C7566] [&>svg]:hover:cursor-pointer">
+            <FacebookIcon />
+            <InstagramIcon />
+            <TwitterIcon />
+            <PinterestIcon />
+          </div>
+        </div>
+        <div className="text-[#2C7566] text-center font-bold mb-2">
+          Challenge by{" "}
+          <a
+            className="focus:outline-none underline decoration-red-400 focus:decoration-yellow-500 decoration-2"
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            className="focus:outline-none underline decoration-red-400 focus:decoration-yellow-500 decoration-2"
+            href="https://www.frontendmentor.io/profile/TusharKaundal"
+            target="_blank"
+          >
+            @TusharKaundal
+          </a>
+          .
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [mobilenav, setMobileNav] = useState(false);
+  return (
+    <header className="absolute z-57 w-full px-4 top-6 md:px-10 font-barlow">
+      <nav className="w-full flex justify-between items-center gap-6">
+        <h1 className="text-[20px] font-bold animate-scaleup">sunnyside</h1>
+
+        <ul className="hidden md:flex items-center gap-2 text-center [&>li]:[&>a:focus]:bg-white [&>li]:[&>a:focus]:font-fraunces [&>li]:[&>a:focus]:font-bold [&>li]:[&>a:focus]:text-[#24303E] [&>li]:animate-movedown ">
+          <li className="focus-within:text-red-400">
+            <a
+              href="#"
+              className="w-[100px] py-3 flex justify-center items-center hover:cursor-pointer hover:bg-white/25 rounded-[48px] focus:outline-none"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="w-[100px] py-3 flex justify-center items-center hover:cursor-pointer hover:bg-white/25 rounded-[48px] focus:outline-none"
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="w-[100px] py-3 flex justify-center items-center hover:cursor-pointer hover:bg-white/25 rounded-[48px] focus:outline-none"
+            >
+              Projects
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="w-[100px] py-3 flex justify-center items-center hover:cursor-pointer hover:bg-white/25 rounded-[48px] focus:outline-none"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="md:hidden size-8 hover:cursor-pointer"
+          onClick={() => setMobileNav((prev) => !prev)}
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </nav>
+
+      {mobilenav && (
+        <div className="md:hidden text-black relative z-20 flex justify-center items-center py-10 mt-5 bg-white w-full overflow-x-clip">
+          <div className="absolute w-6 h-6 bg-white rotate-45 -right-3 -top-3"></div>
+          <ul className="flex flex-col gap-4 [&>li]:[&>a:focus]:bg-yellow-300 text-center">
+            <li>
+              <a
+                className="block  px-6 py-3 hover:cursor-pointer hover:bg-yellow-300 rounded-[48px] focus:outline-none"
+                href="#"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                className="block  px-6 py-3 hover:cursor-pointer hover:bg-yellow-300 rounded-[48px] focus:outline-none"
+                href="#"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                className="block  px-6 py-3 hover:cursor-pointer hover:bg-yellow-300 rounded-[48px] focus:outline-none"
+                href="#"
+              >
+                Project
+              </a>
+            </li>
+            <li>
+              <a
+                className="block  px-6 py-3 hover:cursor-pointer hover:bg-yellow-300 rounded-[48px] focus:outline-none"
+                href="#"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Navbar;
